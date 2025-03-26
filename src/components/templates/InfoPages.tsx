542da8193fd7cb206302676184ad14bb38f391fd
Valentina <valenmarinocol@gmail.com>
2025-03-26 15:47:25 +0100

    temporarily removed info pages footer

--- src/components/templates/InfoPages.tsx
+++ src/components/templates/InfoPages.tsx
@@ -2,7 +2,6 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { ReactNode } from "react";
 import { getChars } from "@/utils/getChars";
-import Image from "next/image";
 
 type InfoPagesTemplateProps = {
   children: ReactNode;
@@ -20,21 +19,6 @@ const InfoPages = ({ children, title }: InfoPagesTemplateProps) => {
       >
         <div className="w-full max-w-[800px] mx-auto text-left z-40 bg-[#f2f2f2] flex fixed md:h-[220px] h-[150px] md:pt-20">
           <div className="text-6xl md:text-9xl flex items-center tracking-tighter text-black/80 fixed pt-20 md:pt-0 pl-3 md:pl-0 overflow-hidden">
-            {/* <Link href="/" className="pr-4 md:hidden flex">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="1"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M19 12H5M12 19l-7-7 7-7" />
-              </svg>
-            </Link> */}
             {getChars(title)}
           </div>
           <div className="flex flex-col ml-auto mt-24 mr-4 mb-auto md:mt-4">
@@ -77,9 +61,8 @@ const InfoPages = ({ children, title }: InfoPagesTemplateProps) => {
           <div className="mt-0 md:mt-32 z-30 overflow-hidden">{children}</div>
         </motion.div>
       </div>
-      {/* <div className="w-full h-24 z-50 bg-gradient-to-b from-transparent from-10% to-[#f2f2f2] to-80% fixed bottom-8"></div> */}
 
-      <div className="w-full text-sm fixed bottom-0 z-50 h-8 ">
+      {/* <div className="w-full text-sm fixed bottom-0 z-50 h-8 ">
         <div className="w-full h-full relative items-center justify-evenly flex">
           <div className="flex justify-evenly items-center w-full h-full z-50">
             <p>email</p>
@@ -98,9 +81,7 @@ const InfoPages = ({ children, title }: InfoPagesTemplateProps) => {
           </div>
         </div>
 
-        {/* {" "}
-        {" "} */}
-      </div>
+      </div> */}
     </>
   );
 };
