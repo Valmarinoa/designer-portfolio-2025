45806040a94f985b58b816c28d984bf373d2a867
Valentina <valenmarinocol@gmail.com>
2025-03-26 16:37:16 +0100

    Update AboutContent.tsx

--- src/components/AboutContent.tsx
+++ src/components/AboutContent.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { MaskText } from "./MaskText";
-import SocialsComponent from "./SocialsComponent";
 import { motion } from "framer-motion";
 import { mediaVariants } from "@/constants/variants";
 
