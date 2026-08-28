366a649148962ff69db6baaf316bc09f1fa73004
Valentina Marino <valenmarinocol@gmail.com>
2026-08-28 14:54:51 +0200

    updated downloadable CV

--- src/components/mobile/MobileLandingPage.tsx
+++ src/components/mobile/MobileLandingPage.tsx
@@ -15,7 +15,7 @@ import Link from "next/link";
 const MobileLandingPage = () => {
   const phrases: React.ReactNode[] = [
     "Hi! I’m an",
-    " Amsterdam-based Desgin Engineer, Experience Strategist and Frontend Developer",
+    " Amsterdam-based Desgin Engineer, Frontend Developer and Experience Strategist",
     <>
       creating{" "}
       <span className="font-baskervville-italic text-[39px]">digital</span> and{" "}
